14696a69df3fb3a84af9d6b5eff683f1020ae447
ChipHennig <chip.hennig@gmail.com>
2021-04-07 14:53:43 -0500

    init commit

---
+++ video_app/main.py
@@ -0,0 +1,75 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+
+# pip install opencv-python for this to work
+import cv2
+
+proto_text = "deploy.prototxt.txt"
+face_extraction_weights = "opencv_face_detector.caffemodel"
+confidence_threshold = 0.8
+
+
+def process_frame(frame):
+
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the detections and predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+
+        # extract the confidence (i.e., probability) associated with the prediction
+        confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
+        if confidence < confidence_threshold:
+            continue
+
+        # compute the (x, y)-coordinates of the bounding box for the object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(proto_text, face_extraction_weights)
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    # print("In loop")
+
+    frame = vs.read()
+    print(type(frame))
+    frame = imutils.resize(frame, width=400)
+    process_frame(frame)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
